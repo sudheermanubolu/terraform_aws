319a4fe28c9fad923821b0a61618ae205c82bc41
Sudheer Manubolu <sudheermanubolu@Sudheers-MacBook-Pro.local>
2020-04-20 23:55:24 -0700

    updated EFS and working

---
+++ EFS.tf
@@ -0,0 +1,31 @@
+resource "aws_efs_file_system" "efs" {
+  throughput_mode  = "bursting"
+  performance_mode = "generalPurpose"
+  encrypted        = "false"
+  tags = {
+    Name = "cacheEFS"
+  }
+}
+
+resource "aws_efs_mount_target" "efs_mount_sub1" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = aws_subnet.main-public-1.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "efs_mount_sub2" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = aws_subnet.main-public-2.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+resource "aws_efs_mount_target" "efs_mount_sub3" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = aws_subnet.main-public-3.id
+  security_groups = [aws_security_group.efs.id]
+}
+
+output "aws_efs_file_system" {
+  value = aws_efs_file_system.efs.id
+}
+
--- SecurityGroups.tf
+++ SecurityGroups.tf
@@ -87,3 +87,26 @@ resource "aws_security_group" "elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+####################################
+#######Security Group for EFS#######
+####################################
+
+##security group for EFS to allow port 2049 ingress and all egress traffic
+resource "aws_security_group" "efs" {
+  name        = "efs-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Security group for EFS"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
